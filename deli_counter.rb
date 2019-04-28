cd2d9ff81803b361f3ad908af386cef7149dea56
Iskra B <ibatistapoblete@gmail.com>
2019-04-28 12:12:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def line(array)
+  if array.any?
+    new_arr = ["The line is currently:"]
+    array.each_with_index {|name, index| new_arr << "#{index+1}. #{name}"}
+    puts new_arr.join(" ")
+  else array = []
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  if array = []
+    array.each_with_index
+  
